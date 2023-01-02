132011ed2428bbb7d8795679f11c19af5e70e76d
Emad_ASUS <e.a.darwich@gmail.com>
2023-01-02 23:12:52 +0100

    add outliers functions

--- Emad/analyse/my_functions.py
+++ Emad/analyse/my_functions.py
@@ -1,6 +1,11 @@
 from matplotlib.cbook import boxplot_stats
+import seaborn as sns
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
-def get_outliers(data):
+
+def get_outliers_length(data):
     # Extract the outlier values from the data
     for i in data.columns:
         if data[i].dtype in ["int64", "float64"]:
@@ -13,3 +18,31 @@ def get_outliers(data):
                 print("Error!!!!!")
                 break
         
+
+def get_outliers_by_column(column):
+  if column.dtype in ["int64", "float64"]:
+          return [y for stat in boxplot_stats(column) for y in stat['fliers']]
+  else:
+    print("Error, data must be int or float")
+
+
+
+
+def boxplot_all_numeric_columns(data):
+    # Select only the numeric columns
+    numeric_data = data.select_dtypes(include=['int64', 'float64'])
+
+    names = list(numeric_data.columns)
+    f, axes = plt.subplots(round(len(names)/2), 2,  figsize=(14, 4))  
+    if numeric_data.shape[1]%2 == 0 :
+        n_lignes = int(numeric_data.shape[1]/2) 
+    else : 
+        n_lignes = int(numeric_data.shape[1]/2+1) 
+    y = 0;
+    for name in names:
+        i, j = divmod(y, n_lignes)
+        sns.boxplot(x=numeric_data[name], ax=axes[i, j])
+        y = y + 1
+
+    plt.tight_layout()
+    plt.show()
